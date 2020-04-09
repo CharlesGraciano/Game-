cb4ea78f2bea6d4706642913a5b3d62205f3c603
CharlesGraciano <55153290+CharlesGraciano@users.noreply.github.com>
2020-04-08 17:17:51 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,39 @@
 var s;
+var scl = 20;
+var food;
+
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(500, 500);
   s = new Snake();
+  frameRate(10);
+  pickLocation();
+}
+function pickLocation(){
+ var cols = floor (width/scl);
+ var rows = floor (height/scl);
+  food = createVector ( floor(random(cols)), floor(random(rows)));
+  food.mult(scl); 
+}
+
+function mousePressed() {
+s.total++;
 }
 
 function draw() {
-  background(10);
+  background(0,200,90);
+ 
+  
+if (s.eat(food)){
+pickLocation();
+}
+  s.death();
   s.update();
   s.show();
+  
+  
+  fill(255,0,100);
+  rect (food.x, food.y, scl,scl);
 }
 
 function keyPressed() {
@@ -16,28 +41,9 @@ function keyPressed() {
     s.dir(0,-1);
   } else if (keyCode === DOWN_ARROW) {
     s.dir(0,1);
-  } else if (keyCode === DOWN_ARROW) {
+  } else if (keyCode === RIGHT_ARROW) {
     s.dir(1,0);
-  } else if (keyCode === DOWN_ARROW) {
+  } else if (keyCode === LEFT_ARROW) {
   s.dir(-1,0);
   }
-}
-
-function Snake() {
-  this.x = 0;
-  this.y = 0;
-  this.xspeed = 1;
-  this.yspeed = 0;
-
-  this.update = function() {
-    this.x = this.x + this.xspeed;
-    this.y = this.y + this.yspeed
-  }
-
-  this.show = function() {
-    fill(255);
-    rect(this.x, this.y, 10, 10);
-  }
-
-
 }
