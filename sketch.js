02fefff7a79dd8044ebfbc7f5ec5c55aeeb65a81
CharlesGraciano <55153290+CharlesGraciano@users.noreply.github.com>
2020-05-06 16:43:54 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,17 @@
 var s;
 var scl = 10;
 var food;
-
+let array = [];
+let noiseOffset = 0.0;
+let strokeWidth = 5;
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(windowWidth, windowHeight);
+  background(200);
+  
+  noFill();
+
+  //createCanvas(500, 500);
   s = new Snake();
   frameRate(10);
   pickLocation();
@@ -21,7 +28,22 @@ s.total++;
 }
 
 function draw() {
-  background(random());
+  //background(random());
+   //if (mouseIsPressed){
+// if (mouseIsPressed==true){
+background(200,0,0,10);
+strokeWeight(strokeWidth);
+
+noiseOffset += 0.07;
+strokeWidth = noise(noiseOffset) * 100;
+
+
+stroke(map(mouseX,0,500,0,300,true));
+line(width-mouseX, height-mouseY, width-pmouseX, height-pmouseY);
+line(mouseX, mouseY, pmouseX, pmouseY);
+//background(0);
+array.push([mouseX,mouseY]);
+
  
   
 if (s.eat(food)){
@@ -40,6 +62,18 @@ pickLocation();
   ellipse (food.y, food.x, scl,scl);
   fill(255,0,100);
   ellipse (food.x, food.x, scl,scl);
+  
+  beginShape();
+for(let i = 0; i < array.length; i++){
+//line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+curveVertex(array[i][0], array[i][1]);
+
+}
+endShape();{
+//line(array[0][0], array[0][1],array[2][0],array[2][1]);
+}
+return false;
+}
   
 }
 
